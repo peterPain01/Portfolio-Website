e0b67b9cf352c11ab575b3c0081341b5fb6ecb7f
21127617 <phamhuy1112003@gmail.com>
2024-05-22 16:53:46 +0700

    add project animation

--- index.js
+++ index.js
@@ -59,3 +59,13 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     await createToolsItem(tools_carousel);
     await createToolsItem(tools_carousel);
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) entry.target.classList.add("show");
+        else entry.target.classList.remove("show");
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
